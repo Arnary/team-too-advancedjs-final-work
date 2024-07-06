c21f63b5a8e2f1e49c69d2ba1a10b684cf25472a
Dmitriy Teplinskiy <99141331+MONSMIN@users.noreply.github.com>
2024-07-06 16:03:13 +0300

    fix footer
    add footer margin top
    import izitoast
    add show message

--- src/js/subscribe.js
+++ src/js/subscribe.js
@@ -1,46 +1,92 @@
-// import { axiosWrapper } from './utils/axiosWrapper.js';
+import { axiosWrapper } from './utils/axiosWrapper.js';
+import 'izitoast/dist/css/iziToast.min.css';
+import iziToast from 'izitoast';
 
-// const api = new axiosWrapper();
-// const BASE_URL = 'https://your-energy.b.goit.study/api/subscription';
+const api = new axiosWrapper();
+const BASE_URL = 'https://your-energy.b.goit.study/api/subscription';
 
-// const form = document.querySelector('.subscribe-form');
-// const emailInput = document.getElementById('email');
+const form = document.querySelector('.subscribe-form');
+const emailInput = document.getElementById('email');
 
-// form.addEventListener('submit', async (event) => {
-//     event.preventDefault();
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-//     const email = emailInput.value;
+    const email = emailInput.value;
 
-//     if (isValidEmail(email)) {
-//         try {
-            
-//             await sendSubscription(email);
-            
-//             alert('Thank you for subscribing!');
-//             emailInput.value = '';
-//         } catch (error) {
-//             console.error('Error:', error);
-//             alert('An error occurred. Please try again.');
-//             emailInput.value = '';
-//         }
-//     } else {
-//         alert('Please enter a valid email address.');
-//     }
-// });
+    if (isValidEmail(email)) {
+        try {
+            const response = await sendSubscription(email);
+            showSuccessToast(response.message);
+            emailInput.value = '';
+        } catch (error) {
+            handleError(error);
+        }
+    } else {
+        showErrorToast(response.message);
+    }
+});
 
-// function isValidEmail(email) {
-//     const regex = /^([\w.%+-]+@([\w.-]+\.[a-zA-Z]{2,}))$/i;
-//     return regex.test(email);
-// }
+function isValidEmail(email) {
+    const regex = /^([\w.%+-]+@([\w.-]+\.[a-zA-Z]{2,}))$/i;
+    return regex.test(email);
+}
 
-// async function sendSubscription(email) {
-//     const url = `${BASE_URL}`;
-//     const data = { email: email };
+async function sendSubscription(email) {
+    const url = `${BASE_URL}`;
+    const data = { email: email };
 
-//     try {
-//         const response = await api.post(url, data);
-//         return response;
-//     } catch (error) {
-//         throw error;
-//     }
-// }
+    try {
+        const response = await api.post(url, data);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+function showSuccessToast(message) {
+    iziToast.success({
+        message: message,
+        position: 'center',
+        timeout: 5000
+    });
+}
+
+function showInfoToast(message) {
+    iziToast.info({
+        message: message,
+        position: 'center',
+        timeout: 5000
+    });
+}
+
+function showErrorToast(message) {
+    iziToast.error({
+        message: message,
+        position: 'center',
+        timeout: 5000
+    });
+}
+
+
+function handleError(error) {
+    if (error.response) {
+        const { status, data } = error.response;
+        switch (status) {
+            case 400:
+                showErrorToast(data.message);
+                break;
+            case 404:
+                showNotFoundToast('Requested resource not found.');
+                break;
+            case 409:
+                showInfoToast('Subscription already exists.');
+                break;
+            case 500:
+                showErrorToast('Server error. Please try again later.');
+                break;
+            default:
+                showInfoToast(data.message);
+                break;
+        }
+    }
+}
